cd9619469b8759fc0ec36e602aed16e695d56659
Caden Atwell <99625358+CAtwell2@users.noreply.github.com>
2022-06-22 02:29:00 -0400

    Add files via upload

---
+++ Restaurantproject.cs
@@ -0,0 +1,17 @@
+﻿
+const double TAX_RATE = 0.0675;
+double mealCost;
+double tax;
+double tip;
+double totalbill;
+
+Console.WriteLine("Enter the total cost of the meal.");
+mealCost = Convert.ToDouble(Console.ReadLine());
+tax = mealCost * TAX_RATE;
+tip = mealCost * 0.2;
+totalbill = mealCost + tax + tip;
+
+Console.WriteLine($"The meal cost is: {mealCost}");
+Console.WriteLine($"The tax is: {tax}");
+Console.WriteLine($"The tip is: {tip}");
+Console.WriteLine($"The total bill is: {totalbill}");
